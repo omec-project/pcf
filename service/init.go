463332203f20ae37b5cfe5227df625ec9379a921
shubham_joshi <shubham.s.joshi@gslab.com>
2021-10-05 17:35:53 +0530

    SDCORE-524 Provisioning Profiling for 5G NFs

--- service/init.go
+++ service/init.go
@@ -8,6 +8,8 @@ package service
 import (
 	"bufio"
 	"fmt"
+	"net/http"
+	_ "net/http/pprof" //Using package only for invoking initialization.
 	"os"
 	"os/exec"
 	"os/signal"
@@ -114,6 +116,11 @@ func (pcf *PCF) Initialize(c *cli.Context) error {
 			ConfigPodTrigger <- true
 		}()
 	}
+	//Initiating a server for profiling
+	go func() {
+		http.ListenAndServe(":5001", nil)
+	}()
+
 	return nil
 }
 
